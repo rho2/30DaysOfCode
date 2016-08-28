af179fcfb17956df20f5ad701de05b43a69bc40d
rho2 <rho2bin@gmail.com>
2016-08-28 21:49:23 +0200

    Day14 - Scope

---
+++ day14.py
@@ -0,0 +1,13 @@
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+    def computeDifference(self):
+        self.maximumDifference = abs(min(self.__elements) - max(self.__elements))
+        
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)
