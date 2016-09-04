2c88437ad66742a3e684f568ef10f7806900b1e3
rho2 <rho2bin@gmail.com>
2016-09-04 18:37:31 +0200

    Day21 - Generics

---
+++ day21.java
@@ -0,0 +1,22 @@
+import java.lang.reflect.Method;
+
+class Solution {
+public static <E> void printArray(E[] generic){
+    for(E element : generic) {
+        System.out.println(element); 
+    }
+}
+
+    
+public static void main(String args[]){
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = { "Hello", "World" };
+        
+        printArray( intArray  );
+        printArray( stringArray );
+        
+        if(Solution.class.getDeclaredMethods().length > 2){
+            System.out.println("You should only have 1 method named printArray.");
+        }
+    }
+}
